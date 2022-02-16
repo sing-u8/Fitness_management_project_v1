810c2552a5f8eb42d4a6f73f23c7d2d6d8c28013
ParkSingyu <singyu13@gmail.com>
2022-02-16 14:54:07 +0900

    - MODIFY : html에서 this 접근자 제거

--- src/app/feature/redwhale/auth/auth-routing.module.ts
+++ src/app/feature/redwhale/auth/auth-routing.module.ts
@@ -1,41 +1,38 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-import { AuthGuard } from "@guards/auth.guard";
-import { TermsGuard } from "@guards/terms.guard";
+import { AuthGuard } from '@guards/auth.guard'
+import { TermsGuard } from '@guards/terms.guard'
 
-import { LoginComponent } from "./login/login.component";
-import { EmailLoginComponent } from "./emailLogin/emailLogin.component";
-import { TermsComponent } from "./terms/terms.component";
-import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
-import { ResetPasswordComponent } from "./reset-password/reset-password.component";
+import { LoginComponent } from './login/login.component'
+import { EmailLoginComponent } from './emailLogin/emailLogin.component'
+import { TermsComponent } from './terms/terms.component'
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
+import { ResetPasswordComponent } from './reset-password/reset-password.component'
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", canActivate: [AuthGuard], component: LoginComponent },
-  { path: "email", canActivate: [AuthGuard], component: EmailLoginComponent },
-  { path: "terms", canActivate: [TermsGuard], component: TermsComponent },
-  {
-    path: "registration",
-    loadChildren: () =>
-      import("./registration/registration.module").then(
-        (m) => m.RegistrationModule
-      ),
-  },
-  {
-    path: "forgot-password",
-    canActivate: [AuthGuard],
-    component: ForgotPasswordComponent,
-  },
-  {
-    path: "reset-password",
-    canActivate: [AuthGuard],
-    component: ResetPasswordComponent,
-  },
-];
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', canActivate: [AuthGuard], component: LoginComponent },
+    { path: 'email', canActivate: [AuthGuard], component: EmailLoginComponent },
+    { path: 'terms', canActivate: [TermsGuard], component: TermsComponent },
+    {
+        path: 'registration',
+        loadChildren: () => import('./registration/registration.module').then((m) => m.RegistrationModule),
+    },
+    {
+        path: 'forgot-password',
+        canActivate: [AuthGuard],
+        component: ForgotPasswordComponent,
+    },
+    {
+        path: 'reset-password',
+        canActivate: [AuthGuard],
+        component: ResetPasswordComponent,
+    },
+]
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class AuthRoutingModule {}
