509890d5fc14b1573e6651add821078302cacef3
ParkSingyu <singyu13@gmail.com>
2022-02-22 09:43:40 +0900

    - MODIFY : 불필요한 개발 부분 제거 - 1

--- src/app/feature/redwhale/auth/common/header/header.component.ts
+++ src/app/feature/redwhale/auth/common/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
-  selector: "rw-auth-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"],
+    selector: 'rw-auth-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
-  goRouterLink(link: string) {
-    this.router.navigateByUrl(link);
-  }
+    goRouterLink(link: string) {
+        this.router.navigateByUrl(link)
+    }
 }
