eb4e32a002bc5c4c334270ceca22e431e18bfabb
ParkSingyu <singyu13@gmail.com>
2022-03-12 01:32:30 +0900

    - FEA : main carousel

--- src/app/feature/homepage/components/main-carousel/main-carousel.component.ts
+++ src/app/feature/homepage/components/main-carousel/main-carousel.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core'
 
+import _ from 'lodash'
+
+type NaviType = {
+    [K in NaviTypeKeys]: boolean
+}
+type NaviTypeKeys = 'reentrance' | 'counselMember' | 'adjustSchedule' | 'manageWorkout'
+
 @Component({
     selector: 'hp-main-carousel',
     templateUrl: './main-carousel.component.html',
     styleUrls: ['./main-carousel.component.scss'],
 })
 export class MainCarouselComponent implements OnInit {
+    public naviObj: NaviType = {
+        reentrance: true,
+        counselMember: false,
+        adjustSchedule: false,
+        manageWorkout: false,
+    }
+
     constructor() {}
 
     ngOnInit(): void {}
+
+    naviTo(prop: NaviTypeKeys) {
+        _.forIn(this.naviObj, (_, key) => {
+            if (prop == key) {
+                this.naviObj[key] = true
+            } else {
+                this.naviObj[key] = false
+            }
+        })
+    }
 }
