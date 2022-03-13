547422246c6240c3564c8b54488c9b78a9641cc5
ParkSingyu <singyu13@gmail.com>
2022-03-13 22:59:45 +0900

    - FEA : main-carousel component

--- src/app/feature/homepage/components/homepage-header/homepage-header.component.ts
+++ src/app/feature/homepage/components/homepage-header/homepage-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Renderer2, OnDestroy } from '@angular/core'
 import { Router } from '@angular/router'
 
 @Component({
@@ -6,12 +6,40 @@ import { Router } from '@angular/router'
     templateUrl: './homepage-header.component.html',
     styleUrls: ['./homepage-header.component.scss'],
 })
-export class HomepageHeaderComponent implements OnInit {
-    constructor(private router: Router) {}
+export class HomepageHeaderComponent implements OnInit, OnDestroy {
+    public isMenuOpen = false
+    public resizeListener: () => void
 
-    ngOnInit(): void {}
+    public isMobileWidth = false
+
+    constructor(private router: Router, private renderer: Renderer2) {}
+
+    ngOnInit(): void {
+        this.checkIsMobile(window.innerWidth)
+        this.resizeListener = this.renderer.listen('window', 'resize', (event) => {
+            if (event.target.innerWidth >= 960 && this.isMenuOpen) {
+                this.isMenuOpen = false
+            }
+            this.checkIsMobile(event.target.innerWidth)
+        })
+    }
+    ngOnDestroy(): void {
+        this.resizeListener()
+    }
 
     navigateTo(url: string) {
         this.router.navigateByUrl(url)
+    }
+
+    toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen
+    }
+
+    checkIsMobile(width) {
+        if (width >= 768 && this.isMobileWidth) {
+            this.isMobileWidth = false
+        } else if (width < 768 && !this.isMobileWidth) {
+            this.isMobileWidth = true
+        }
     }
 }
--- src/app/feature/homepage/components/main-carousel/main-carousel.component.ts
+++ src/app/feature/homepage/components/main-carousel/main-carousel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, HostListener, ViewChild, ElementRef, Renderer2 } from '@angular/core'
+import { state, style, trigger } from '@angular/animations'
 
 import _ from 'lodash'
 
@@ -11,6 +12,38 @@ type NaviTypeKeys = 'reentrance' | 'counselMember' | 'adjustSchedule' | 'manageW
     selector: 'hp-main-carousel',
     templateUrl: './main-carousel.component.html',
     styleUrls: ['./main-carousel.component.scss'],
+    animations: [
+        trigger('imageIndicator', [
+            state(
+                'move',
+                style({
+                    transform: 'translate(calc({{imgIdx}} / {{imgListLen}} * -100%))',
+                    transition: 'transform 0.3s ease-out',
+                }),
+                { params: { imgIdx: 0, imgListLen: 1 } }
+            ),
+        ]),
+        trigger('onDrag', [
+            state(
+                'dragOn',
+                style({
+                    transform: 'translate(calc( {{i}} / {{n}} * -100% + {{tx}}px))',
+                }),
+                { params: { i: 1, n: 1, tx: 0 } }
+            ),
+        ]),
+        trigger('autoScroll', [
+            state(
+                'scrolling',
+                style({
+                    transform: 'translate(calc({{imgIdx}} / {{imgListLen}} * -100%))',
+                    transition: 'transform 0.3s ease-out',
+                }),
+                { params: { imgIdx: 0, imgListLen: 1 } }
+            ),
+            state('stop', style({})),
+        ]),
+    ],
 })
 export class MainCarouselComponent implements OnInit {
     public naviObj: NaviType = {
@@ -20,11 +53,56 @@ export class MainCarouselComponent implements OnInit {
         manageWorkout: false,
     }
 
-    constructor() {}
+    @ViewChild('images') images_el: ElementRef
+
+    constructor(private renderer: Renderer2) {}
 
     ngOnInit(): void {}
 
-    naviTo(prop: NaviTypeKeys) {
+    public imageIdx = 0
+    public imageLength = 4
+
+    public isOnOtherWork = false
+    public isTimerSet = false
+    public autoScrollTimer
+
+    isAutuScrollAvailable() {
+        return !this.isOnOtherWork
+    }
+    startAutoScroll() {
+        if (!this.isTimerSet) {
+            this.autoScrollTimer = setInterval(() => {
+                this.imageIdx = (this.imageIdx + 1) % this.imageLength
+                // this.exportImageIdx(this.imageIdx)
+                const [curIndex, curNav] = this.checkNavImage()
+                this.naviTo(curNav)
+            }, 10000)
+            this.isTimerSet = true
+        }
+    }
+    stopAutoScroll() {
+        clearInterval(this.autoScrollTimer)
+        this.isTimerSet = false
+    }
+    setAutoScroll() {
+        this.isAutuScrollAvailable() ? this.startAutoScroll() : this.stopAutoScroll()
+    }
+
+    @HostListener('mouseenter', ['$event'])
+    onMouseEnter(e) {
+        this.isOnOtherWork = true
+        this.stopAutoScroll()
+    }
+
+    @HostListener('mouseleave', ['$event'])
+    onMouseLeave(e) {
+        this.isOnOtherWork = false
+        this.setAutoScroll()
+    }
+
+    // --------------------------
+
+    naviTo(prop: NaviTypeKeys, auto = true) {
         _.forIn(this.naviObj, (_, key) => {
             if (prop == key) {
                 this.naviObj[key] = true
@@ -32,5 +110,25 @@ export class MainCarouselComponent implements OnInit {
                 this.naviObj[key] = false
             }
         })
+
+        if (auto == false) {
+            const navObjKeys = _.keys(this.naviObj)
+            const curIndex = _.findIndex(navObjKeys, (v, i) => v == prop)
+
+            this.imageIdx = curIndex
+            // this.renderer.setStyle(
+            //     this.images_el.nativeElement,
+            //     'transform',
+            //     `translate(calc( ${curIdx} / ${this.imageLength} * -100%))`
+            // )
+        }
+    }
+
+    // check nav image
+    checkNavImage(): [number, NaviTypeKeys] {
+        const navObjKeys = _.keys(this.naviObj)
+        const curIndex = _.findIndex(navObjKeys, (v, i) => i == this.imageIdx)
+
+        return [curIndex, navObjKeys[curIndex] as NaviTypeKeys]
     }
 }
