61e6c0dcfb0d606735477685c320534d1c0e4ef9
ParkSingyu <singyu13@gmail.com>
2022-03-17 00:24:20 +0900

    .

--- src/app/feature/homepage/main/main.component.ts
+++ src/app/feature/homepage/main/main.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Renderer2, OnDestroy, AfterViewInit } from '@angular/core'
 
 @Component({
     selector: 'rw-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-    constructor() {}
+export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
+    constructor(private renderer: Renderer2) {}
 
     ngOnInit(): void {}
+    ngOnDestroy(): void {}
+    ngAfterViewInit(): void {
+        const hpScrollTag = document.querySelectorAll('.hp-scroll-ani')
+        console.log('hpScrooTags : ', hpScrollTag)
+    }
 
     public receiveIntroVisible = false
     onReceiveIntroClose() {
@@ -20,4 +25,7 @@ export class MainComponent implements OnInit {
     onReceiveIntroOpen() {
         this.receiveIntroVisible = true
     }
+
+    // animation funcs and vals
+    public animationListener = undefined
 }
