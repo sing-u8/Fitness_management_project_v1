57d717b35e601aef41aac261672fca832e86d1da
ParkSingyu <singyu13@gmail.com>
2022-03-17 18:37:24 +0900

    - FEA : init locker.component

--- src/app/feature/redwhale/center/section/locker/locker.component.ts
+++ src/app/feature/redwhale/center/section/locker/locker.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./locker.component.scss'],
 })
 export class LockerComponent implements OnInit {
+    // <----
+    public isEditMode = false
+
+    // ----->
+
     constructor() {}
 
     ngOnInit(): void {}
