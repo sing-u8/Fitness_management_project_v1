e7cf021e2194958d9f8b7504d6e2fb945760c204
ParkSingyu <singyu13@gmail.com>
2022-03-21 10:05:26 +0900

    - FEA : locker-tip-dropdown 수정

--- src/app/feature/redwhale/center/section/locker/components/locker-tip-dropdown/locker-tip-dropdown.component.ts
+++ src/app/feature/redwhale/center/section/locker/components/locker-tip-dropdown/locker-tip-dropdown.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core'
 export class LockerTipDropdownComponent implements OnInit {
     @Input() title: string
 
-    public isDropDownOpen: boolean
+    public isDropDownOpen = false
     constructor() {}
 
     ngOnInit(): void {}
