5247d358f6a143b8cec746cf8ba0edf3a785844b
ParkSingyu <singyu13@gmail.com>
2022-04-13 18:10:10 +0900

    - FEA : 도입 문의 작업중 (footer 부분 완성)

--- src/app/feature/homepage/components/free-start-footer/free-start-footer.component.ts
+++ src/app/feature/homepage/components/free-start-footer/free-start-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core'
 
 @Component({
     selector: 'hp-free-start-footer',
@@ -7,6 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 })
 export class FreeStartFooterComponent implements OnInit {
     @Output() onFreeStartClick = new EventEmitter<void>()
+    @Input() animation = false
     constructor() {}
 
     ngOnInit(): void {}
