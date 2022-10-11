5b37dccf4fe4904c71b3cb151f4309bdd203f20f
ParkSingyu <singyu13@gmail.com>
2022-10-11 16:53:26 +0900

    -- MODIFY: sms-history-group -- state_code 타입 수정

--- src/app/core/schemas/sms-history-group.ts
+++ src/app/core/schemas/sms-history-group.ts
@@ -1,7 +1,7 @@
 export interface SMSHistoryGroup {
     id: string
     datetime: string
-    state_code: string
+    state_code: 'sms_state_ready' | 'sms_state_sent'
     state_code_name: string
     type_code: string
     type_code_name: string
