b075d0611477c93a88af3a62680ddc881030b799
ParkSingyu <singyu13@gmail.com>
2022-12-23 11:55:18 +0900

    -- DEBUG : word-ellipsis pipe 코드 재수정

--- src/app/shared/components/redwhale/community/chatting-room-card/chatting-room-card.component.ts
+++ src/app/shared/components/redwhale/community/chatting-room-card/chatting-room-card.component.ts
@@ -34,7 +34,8 @@ export class ChattingRoomCardComponent implements OnInit, AfterViewInit, OnChang
     ngOnInit(): void {
         this.initUserListInScreen()
     }
-    ngAfterViewInit(): void {}
+    ngAfterViewInit(): void {
+    }
     ngOnChanges(): void {}
 
     initUserListInScreen() {
@@ -55,12 +56,12 @@ export class ChattingRoomCardComponent implements OnInit, AfterViewInit, OnChang
                 : this.room.chat_room_users.filter((v) => v.id != this.curUser.id).map((v) => v)
     }
 
-    // limitChatRoomName() {
-    //     if (this.room.chat_room_users.length == 1) {
-    //         this.chatRoomName = _.filter(this.room.chat_room_users, (v) => v.id != this.curUser.id)[0].name
-    //     } else {
-    //         const userNames = _.split(this.room.name, ', ', 3)
-    //         this.chatRoomName = userNames.length > 2 ? `${userNames[0]}, ${userNames[1]}, ...` : this.room.name
-    //     }
-    // }
+    limitChatRoomName() {
+        // if (this.room.chat_room_users.length == 1) {
+        //     this.chatRoomName = _.filter(this.room.chat_room_users, (v) => v.id != this.curUser.id)[0].name
+        // } else {
+        //     const userNames = _.split(this.room.name, ', ', 3)
+        //     this.chatRoomName = userNames.length > 2 ? `${userNames[0]}, ${userNames[1]}, ...` : this.room.name
+        // }
+    }
 }
--- src/app/shared/pipes/word-ellipsis.pipe.ts
+++ src/app/shared/pipes/word-ellipsis.pipe.ts
@@ -1,10 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core'
+import _ from 'lodash'
 
 @Pipe({
     name: 'wordEllipsis',
 })
 export class WordEllipsisPipe implements PipeTransform {
-    transform(value: string, ellipsisTo: number): unknown {
+    transform(value: string, ellipsisTo: number): string {
         if (value && value.length > ellipsisTo) {
             return value.slice(0, ellipsisTo) + '...'
         } else {
