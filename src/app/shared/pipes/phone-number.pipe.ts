67157ca0ee27b02ba6a24148182c36bc0426f3fd
ParkSingyu <singyu13@gmail.com>
2023-02-09 18:15:53 +0900

    -- MODIFY : PhoneNumberPipe 수정

--- src/app/shared/pipes/phone-number.pipe.ts
+++ src/app/shared/pipes/phone-number.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core'
+import _ from 'lodash'
 
 @Pipe({
     name: 'phoneNumber',
 })
 export class PhoneNumberPipe implements PipeTransform {
     transform(value: unknown, ...args: unknown[]): unknown {
+        if (_.isEmpty(value) || value == '-') {
+            return '-'
+        }
         let phoneNum: string = String(value).replace(/\D[^\.]/g, '')
         if (phoneNum.length < 11) {
             phoneNum = phoneNum.slice(0, 3) + '-' + phoneNum.slice(3, 6) + '-' + phoneNum.slice(6)
