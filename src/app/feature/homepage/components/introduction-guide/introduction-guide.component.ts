fea869143a1ebad3bd7b3c05be134073bce4b77d
ParkSingyu <singyu13@gmail.com>
2023-02-17 17:04:44 +0900

    -- DEBUG : 홈페이지 디버깅

--- src/app/feature/homepage/components/introduction-guide/introduction-guide.component.ts
+++ src/app/feature/homepage/components/introduction-guide/introduction-guide.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
-  selector: 'rw-introduction-guide',
-  templateUrl: './introduction-guide.component.html',
-  styleUrls: ['./introduction-guide.component.scss']
+    selector: 'rw-introduction-guide',
+    templateUrl: './introduction-guide.component.html',
+    styleUrls: ['./introduction-guide.component.scss'],
 })
 export class IntroductionGuideComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    public uriForGoogleForm = 'https://forms.gle/cUznbtQzYngJ7TUg9'
+    openUri(uri: string) {
+        window.open(uri)
+    }
 }
